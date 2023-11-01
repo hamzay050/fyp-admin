b99fe8a8eee27612d0d4652ee8bf0b4594f18218
Jamshaid Ahmed Khan <official.jimmi666@gmail.com>
2023-11-02 00:12:08 +0500

    Updated Dashboard

--- components/Admin/Dashboard/index.js
+++ components/Admin/Dashboard/index.js
@@ -4,79 +4,53 @@ import VaccinesIcon from '@mui/icons-material/Vaccines';
 import GroupsIcon from '@mui/icons-material/Groups';
 import PersonIcon from '@mui/icons-material/Person';
 import {Box,Typography} from '@mui/material'
+import UpcomingIcon from '@mui/icons-material/Upcoming';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import PeopleIcon from '@mui/icons-material/People';
+import GridViewIcon from '@mui/icons-material/GridView';
 
 export default function Dashboard() {
   return (
     <>
-      <Typography variant='body1' sx={{color:'#3c3e3ee6',fontWeight:'600',margin:'1rem'}}>Overall Records</Typography>  
-    <Box m='1rem 0' sx={{display:'flex',justifyContent:'space-evenly'}}>
-    <Box sx={{width:'230px',height:'105px',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'space-evenly',border:'1px solid #d5d3d3',borderRadius:'5px'}}>
-        <Box>
-          <VaccinesIcon fontSize='medium' color='secondary'/>
+    <Typography sx={{margin:'1.5rem 0.5rem',fontWeight:600,color:'#504e4e'}}>Dashboard</Typography>
+    <Box sx={{display:'flex'}}>
+     <Box sx={{width:'252px',border:'1px solid #ddd8d8',borderRadius:'9px',boxShadow:1,height:'110px',margin:' 0.5rem',backgroundColor:'white'}}>
+        <Box sx={{width:'50px',height:'50px',border:'1px solid #26a69a',borderRadius:'9px',position:'relative',left:'6%',bottom:'14%',display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'black'}}>
+          <UpcomingIcon fontSize='medium' sx={{color:'#fff'}}/>
         </Box>
-        <Box sx={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-      <Typography variant='body1'sx={{fontWeight:'600'}}>2000</Typography>
-        <Typography variant='caption'>Doctor Entrolled</Typography>
-
+        <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'end',margin:'0 1rem'}}>
+          <Typography variant='body2' sx={{color:'#9fa5a3',fontWeight:600}}>Total Doctor</Typography>
+          <Typography variant='body1' sx={{fontWeight:600,color:'#4f5050'}}>3000</Typography>
         </Box>
-      </Box>
-      <Box sx={{width:'230px',height:'105px',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'space-evenly',border:'1px solid #d5d3d3',borderRadius:'5px'}}>
-        <Box>
-          <PersonIcon fontSize='medium' color='secondary'/>
+     </Box>
+     <Box sx={{width:'252px',border:'1px solid #ddd8d8',borderRadius:'9px',boxShadow:1,height:'110px',margin:' 0.5rem',backgroundColor:'white'}}>
+        <Box sx={{width:'50px',height:'50px',border:'1px solid #26a69a',borderRadius:'9px',position:'relative',left:'6%',bottom:'14%',display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'#4b4bdbd1'}}>
+          <BarChartIcon fontSize='medium' sx={{color:'#fff'}}/>
         </Box>
-        <Box sx={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-      <Typography variant='body1'sx={{fontWeight:'600'}}>2000</Typography>
-        <Typography variant='caption'>Patient Registered</Typography>
-
+        <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'end',margin:'0 1rem'}}>
+          <Typography variant='body2' sx={{color:'#9fa5a3',fontWeight:600}}>Total Doctor</Typography>
+          <Typography variant='body1' sx={{fontWeight:600,color:'#4f5050'}}>3000</Typography>
         </Box>
-      </Box>
-      <Box sx={{width:'230px',height:'105px',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'space-evenly',border:'1px solid #d5d3d3',borderRadius:'5px'}}>
-        <Box>
-          <GroupsIcon fontSize='medium' color='secondary'/>
+     </Box>
+     <Box sx={{width:'252px',border:'1px solid #ddd8d8',borderRadius:'9px',boxShadow:1,height:'110px',margin:' 0.5rem',backgroundColor:'white'}}>
+        <Box sx={{width:'50px',height:'50px',border:'1px solid #26a69a',borderRadius:'9px',position:'relative',left:'6%',bottom:'14%',display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'#2ba92bd1'}}>
+          <GridViewIcon fontSize='medium' sx={{color:'#fff'}}/>
         </Box>
-        <Box sx={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-      <Typography variant='body1'sx={{fontWeight:'600'}}>2000</Typography>
-        <Typography variant='caption'>Total Appointments</Typography>
-
+        <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'end',margin:'0 1rem'}}>
+          <Typography variant='body2' sx={{color:'#9fa5a3',fontWeight:600}}>Total Doctor</Typography>
+          <Typography variant='body1' sx={{fontWeight:600,color:'#4f5050'}}>3000</Typography>
         </Box>
-      </Box>
-
-    </Box>
-    <Typography variant='body1' sx={{color:'#3c3e3ee6',fontWeight:'600',margin:'1rem'}}>Weekly Records</Typography>  
-    <Box m='1rem 0' sx={{display:'flex',justifyContent:'space-evenly'}}>
-    <Box sx={{width:'230px',height:'105px',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'space-evenly',border:'1px solid #d5d3d3',borderRadius:'5px'}}>
-        <Box>
-          <VaccinesIcon fontSize='medium' color='secondary'/>
+     </Box>
+     <Box sx={{width:'252px',border:'1px solid #ddd8d8',borderRadius:'9px',boxShadow:1,height:'110px',margin:' 0.5rem',backgroundColor:'white'}}>
+        <Box sx={{width:'50px',height:'50px',border:'1px solid #df3f5bc2',borderRadius:'9px',position:'relative',left:'6%',bottom:'14%',display:'flex',justifyContent:'center',alignItems:'center',backgroundColor:'#df3f5bc2'}}>
+          <PeopleIcon fontSize='medium' sx={{color:'#fff'}}/>
         </Box>
-        <Box sx={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-      <Typography variant='body1'sx={{fontWeight:'600'}}>2000</Typography>
-        <Typography variant='caption'>Doctor Entrolled</Typography>
-
+        <Box sx={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'end',margin:'0 1rem'}}>
+          <Typography variant='body2' sx={{color:'#9fa5a3',fontWeight:600}}>Total Doctor</Typography>
+          <Typography variant='body1' sx={{fontWeight:600,color:'#4f5050'}}>3000</Typography>
         </Box>
-      </Box>
-      <Box sx={{width:'230px',height:'105px',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'space-evenly',border:'1px solid #d5d3d3',borderRadius:'5px'}}>
-        <Box>
-          <PersonIcon fontSize='medium' color='secondary'/>
-        </Box>
-        <Box sx={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-      <Typography variant='body1'sx={{fontWeight:'600'}}>2000</Typography>
-        <Typography variant='caption'>Patient Registered</Typography>
-
-        </Box>
-      </Box>
-      <Box sx={{width:'230px',height:'105px',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'space-evenly',border:'1px solid #d5d3d3',borderRadius:'5px'}}>
-        <Box>
-          <GroupsIcon fontSize='medium' color='secondary'/>
-        </Box>
-        <Box sx={{display:'flex',alignItems:'center',flexDirection:'column'}}>
-      <Typography variant='body1'sx={{fontWeight:'600'}}>2000</Typography>
-        <Typography variant='caption'>Total Appointments</Typography>
-
-        </Box>
-      </Box>
-
-    </Box>
-    <HeroSection/>
+     </Box>
+     </Box>
     </>
   )
 }
