dcaf2e8f32285f791f6312369464b8546a8a661f
Jamshaid Ahmed Khan <official.jimmi666@gmail.com>
2024-02-11 23:57:55 +0500

    Updated layout with routes

--- layout/index.js
+++ layout/index.js
@@ -1,35 +1,13 @@
-import jwt from "jsonwebtoken"
-import cookies from "cookie"
-import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { Grid } from "@mui/material"
 import Sidebar from "@/components/Sidebar"
 
-export default function index({children}) {
-    const [showSideBar, setShowSideBar] = useState(false)
+export default function SideLayout({children}) {
     const router= useRouter();
-    useEffect(()=>{
-        const cookie= cookies.parse(document.cookie);
-        const token= cookie['accessToken']
-        if(!token){
-              router.push('/')
-        }
-        try {
-           const verifyToken= jwt.verify(token, "mysecret%$sha256/!alpha$%$bang.etae")
-           if(verifyToken){
-               setShowSideBar(true)
-           }else{
-            setShowSideBar(false)
-           }
-            
-        } catch (error) {
-            
-        }
-    })
   return (
     <>
       {
-        showSideBar ? 
+        router.pathname!=='/' ? 
         (<Grid container>
   
        <Grid item xs={2.4}>
