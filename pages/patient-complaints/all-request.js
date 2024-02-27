55b1ddf6670f4abe89147f94a7fb3166af4e0b6b
Jamshaid Ahmed Khan <official.jimmi666@gmail.com>
2024-02-28 01:24:05 +0500

    added patient complaints system

---
+++ pages/patient-complaints/all-request.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AllComplaints from "@/components/AllComplaints"
+
+export default function allRequest() {
+  return (
+    <>
+      <AllComplaints/>
+    </>
+  )
+}
